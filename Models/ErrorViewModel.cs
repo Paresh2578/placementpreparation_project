d277092f1cf287bd37185ada3df87caeb577a246
chaudharyparesh <coding848@gmail.com>
2024-11-11 21:05:33 +0530

    navbar done

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Placement_Preparation.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
